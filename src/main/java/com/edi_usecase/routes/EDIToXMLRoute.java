22a5c40fb05826b625b632af0a2727c1176c20e4
Saurav <saurav.sanyal@bridgesgi.com>
2020-02-28 11:06:33 +0530

    Apache Camel to Kafaka is a success

--- src/main/java/com/edi_usecase/routes/EDIToXMLRoute.java
+++ src/main/java/com/edi_usecase/routes/EDIToXMLRoute.java
@@ -7,8 +7,16 @@ public class EDIToXMLRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		
+		String topicName = "topic=InitialTopic";
+		String kafkaServer = "kafka:localhost:9092";
+		String zooKeeperHost = "zookeeperHost=localhost&zookeeperPort=2181";
+		String serializerClass = "serializerClass=kafka.serializer.StringEncoder";
+
+		String toKafka = new StringBuilder().append(kafkaServer).append("?").append(topicName).append("&")
+				.append(zooKeeperHost).append("&").append(serializerClass).toString();
+
 		from("file:D://inputFolder").to("smooks://smooks-config.xml")
-		.to("xslt:transform.xslt").to("file:D://outputFolder?fileName=outputFile.xml");
+		.to((String)"xslt:transform.xslt").split().tokenize("\n").to(toKafka);
 		
 	}
 }
